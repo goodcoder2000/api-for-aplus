4b95398673652cf729df431a68a97c2381a8dbb2
goodcoder2000 <goodcoder2000@gmail.com>
2022-07-15 20:34:34 +0630

    4api

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { ObjectId } = require('mongodb');
 const MongoClient = require("mongodb").MongoClient;
 const app = express();
+
+app.use(express.json())
+app.use(cors({
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+}));
 
 
 app.listen(process.env.PORT, (req, res) =>{
@@ -15,9 +25,72 @@ MongoClient.connect(url, (err, client) =>{
     db = client.db('aplus');
 })
 
-app.get('/api/shoplists', (req, res) =>{
-    db.collection('shoplists').find().toArray((err, result) =>{
+// get shop category
+
+app.get('/api/shoplists/:category', (req, res) =>{
+    const category = req.params.category;
+    
+    if(category === "bakey"){
+        db.collection('shoplists').find({"category": "bakey"}).toArray((err, result) =>{
+            res.status(200).json(result)
+        })
+    } else if(category === "drink"){
+        db.collection('shoplists').find({"category": "drink"}).toArray((err, result) =>{
+            res.status(200).json(result)
+        })
+    } else if(category === "noddle"){
+        db.collection('shoplists').find({"category": "noddle"}).toArray((err, result) =>{
+            res.status(200).json(result)
+        })
+    } else if(category === "atoke"){
+        db.collection('shoplists').find({"category": "atoke"}).toArray((err, result) =>{
+            res.status(200).json(result)
+        })
+    } else if(category === "akin"){
+        db.collection('shoplists').find({"category": "akin"}).toArray((err, result) =>{
+            res.status(200).json(result)
+        })
+    } else if(category === "chicken"){
+        db.collection('shoplists').find({"category": "chicken"}).toArray((err, result) =>{
+            res.status(200).json(result)
+        })
+    }
+}
+)  
+
+// get singel shop
+
+app.get('/api/shoplists/:id', (req, res) =>{
+    const id = req.params.id;
+
+    db.collection('shoplists').findOne({_id: ObjectId(id)})
+    .then((result) =>{
         res.status(200).json(result)
     })
+})
+
+// add new shop
+
+app.post('/api/shoplists', (req, res) =>{
+    const data = req.body;
+    db.collection('shoplists').insertOne(data)
+    .then((result) =>{
+        res.status(201).json(result)
+    })
+})
+
+// add new menu for shop
+
+app.patch('/api/shoplists/:id/:method', (req, res) =>{
+    const id = req.params.id;
+    const method = req.params.method;
+    const data = req.body;
+
+    if(method === "push"){
+        db.collection('shoplists').updateOne({_id: ObjectId(id)}, {$addToSet: {menu: data}})
+        .then((result) =>{
+            res.status(201).json(result)
+        })
+    }
 })
 
